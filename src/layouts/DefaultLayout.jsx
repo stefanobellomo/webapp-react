e4495977b203163620de6d9c335013c0b1b36cca
stefanobellomo <stefanobellomo1998@gmail.com>
2025-12-11 13:29:03 +0100

    map movie for create a card movie

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -6,7 +6,9 @@ export default function DefaultLayout() {
     return (
         <>
             <Header />
-            <Outlet />
+            <main>
+                <Outlet />
+            </main>
             <Footer />
         </>
     )
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -8,27 +8,28 @@ export default function Homepage() {
     useEffect(() => {
         axios.get('http://localhost:3007/api/movies')
             .then(res => {
-                setMovies(res)
+                setMovies(res.data.movies)
             })
     }, [])
 
+    console.log(movies);
+
     return (
-        <Outlet>
-            <section>
-                <div className="container">
-                    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
-                        { }
-                        <div className="col">
+        <section>
+            <div className="container my-5">
+                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
+                    {movies.map(movie => (
+                        <div className="col" key={movie.id}>
                             <div className="card">
-                                <img src="" alt="cover" className="card-img-top" />
-                            </div>
-                            <div>
-                                <h1></h1>
+                                <img src={`https://placehold.co/200x200?text=${movie.title}`} alt="cover" className="card-img-top" />
+                                <h1>{movie.title}</h1>
+                                <p>{movie.director}</p>
+                                <p>{movie.genre}</p>
                             </div>
                         </div>
-                    </div>
+                    ))}
                 </div>
-            </section>
-        </Outlet>
+            </div>
+        </section>
     )
 }
