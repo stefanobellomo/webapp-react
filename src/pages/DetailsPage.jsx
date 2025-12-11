cc319adff382a163023ee157d36015596b581da6
stefanobellomo <stefanobellomo1998@gmail.com>
2025-12-11 17:02:00 +0100

    add container in details page

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -22,41 +22,42 @@ export default function DetailsPage() {
 
     return (
         <>
+            <div className="container my-4">
+                <section className="movie">
 
-            <section className="movie">
-
-                <div className="col" key={movies.id}>
-                    <div className="card">
-                        <img src={`https://placehold.co/200x200?text=${movies.title}`} alt="cover" className="card-img-top" />
-                    </div>
-                    <div className="card-body p-2">
-                        <h1>Title: {movies.title}</h1>
-                        <h6>Director: {movies.director}</h6>
-                        <h6>Genre: {movies.genre}</h6>
-                        <h6>Relase year: {movies.release_year}</h6>
-                        <h6>Plot: {movies.abstract}</h6>
-                    </div>
-                </div>
-
-            </section>
-
-            <section className="review">
-
-                {reviews.map(review => (
-                    <div className="col" key={review.id}>
+                    <div className="col" key={movies.id}>
+                        <div className="card">
+                            <img src={`https://placehold.co/200x200?text=${movies.title}`} alt="cover" className="card-img-top" />
+                        </div>
                         <div className="card-body p-2">
-                            <h3>Author: {review.name}</h3>
-                            <h6>Vote: <span className={review.vote > 3 ? 'text-success' : 'text-danger'}>{review.vote}</span></h6>
-                            <h6>Text: {review.text}</h6>
-                            <h6>Create at: {review.created_at}</h6>
-                            <h6>Updated at: {review.updated_at}</h6>
+                            <h1>Title: {movies.title}</h1>
+                            <h6>Director: {movies.director}</h6>
+                            <h6>Genre: {movies.genre}</h6>
+                            <h6>Relase year: {movies.release_year}</h6>
+                            <h6>Plot: {movies.abstract}</h6>
                         </div>
                     </div>
-                ))}
 
-                <button className="btn btn-dark" onClick={() => (navigate(-1))}>Torna indietro</button>
+                </section>
 
-            </section>
+                <section className="review">
+
+                    {reviews.map(review => (
+                        <div className="col" key={review.id}>
+                            <div className="card-body p-2">
+                                <h3>Author: {review.name}</h3>
+                                <h6>Vote: <span className={review.vote > 3 ? 'text-success' : 'text-danger'}>{review.vote}</span></h6>
+                                <h6>Text: {review.text}</h6>
+                                <h6>Create at: {review.created_at}</h6>
+                                <h6>Updated at: {review.updated_at}</h6>
+                            </div>
+                        </div>
+                    ))}
+
+                    <button className="btn btn-dark" onClick={() => (navigate(-1))}>Torna indietro</button>
+
+                </section>
+            </div>
 
         </>
     )
