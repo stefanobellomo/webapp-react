10c989d34e5ebfb7bd673b2ebdf821dd501d3b16
stefanobellomo <stefanobellomo1998@gmail.com>
2025-12-11 12:28:55 +0100

    import Outlet

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,5 +1,6 @@
 import Header from "../Components/Header"
 import Footer from "../Components/Footer"
+import { Outlet } from "react-router-dom"
 
 export default function DefaultLayout() {
     return (
