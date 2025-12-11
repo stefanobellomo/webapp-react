e055050959a1681552404697bd6b7621539a5012
stefanobellomo <stefanobellomo1998@gmail.com>
2025-12-11 12:47:13 +0100

    set res in setMovies

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,20 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+
 export default function Homepage() {
+
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:3007/api/movies')
+            .then(res => {
+                setMovies(res)
+            })
+    }, [])
+
     return (
         <main>
-            <h1>ciao homepage</h1>
+
         </main>
     )
 }
