18c846152574f29d8aa7b597c9698f2e9b89705d
stefanobellomo <stefanobellomo1998@gmail.com>
2025-12-12 12:48:05 +0100

    fix image in detailsCard

--- src/Components/DetailsCard.jsx
+++ src/Components/DetailsCard.jsx
@@ -1,15 +1,15 @@
 export default function DetailsCard({ movies }) {
     return (
-        <div className="col d-flex" key={movies.id}>
+        <div className="col d-flex align-items-center" key={movies.id}>
             <div className="card">
-                <img src={`https://placehold.co/200x200?text=${movies.title}`} alt="cover" className="card-img-top " />
+                <img src={`../../public/movies_cover/${movies.image}`} alt="cover" className="card-img-top" />
             </div>
             <div className="card-body p-2">
                 <h1>Title: {movies.title}</h1>
-                <h6>Director: {movies.director}</h6>
-                <h6>Genre: {movies.genre}</h6>
-                <h6>Relase year: {movies.release_year}</h6>
-                <h6>Plot: {movies.abstract}</h6>
+                <h6><i className="bi bi-person-video3 me-2"></i> {movies.director}</h6>
+                <h6><i className="bi bi-tags me-2"></i> {movies.genre}</h6>
+                <h6><i className="bi bi-calendar-event me-2"></i> {movies.release_year}</h6>
+                <h6><i className="bi bi-card-text me-2"></i> {movies.abstract}</h6>
             </div>
         </div>
     )
